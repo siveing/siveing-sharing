dc87419bab841a88da020572c83f3de8def0b4dd
Siveing <siveing.huyy@gmail.com>
2023-01-04 15:34:30 +0700

    [Added] Created page for demo

--- test-folder/index.js
+++
@@ -1,8 +0,0 @@
-const { getTelegramUrl,
-    telegramShare,
-    testing } = require('siveing-sharing');
-
-let objectShare = { url: 'https://stackoverflow.com/questions/16631064/declare-multiple-module-exports-in-node-js', title: '' };
-
-console.log(getTelegramUrl(objectShare));
-console.log(testing('siveing'));
